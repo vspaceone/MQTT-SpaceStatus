a3fa7124af6e77a95cef789fda58c8b289f54e4e
djesionek <Damian.Jes@gmx.de>
2018-02-15 12:56:20 +0100

    Create config.h
    For travis builds

---
+++ src/config.h
@@ -0,0 +1,6 @@
+#define wifi_ssid "your ssid"
+#define wifi_password "your psk"
+
+#define mqtt_server "your mqtt server"
+#define mqtt_user "your mqtt username"
+#define mqtt_password "your mqtt password"

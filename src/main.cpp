b844a9dabf42960f9060d2f5f892995e235cd2fb
H3wastooshort <hacker3000@posteo.org>
2023-07-30 19:33:28 +0200

    debounce switch

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-#include "config.h"
+#include "config.template.h"
 #include "neopixel.h"
 #include "state.h"
 
@@ -18,8 +18,6 @@ NeopixelInterface interface(&state);
 
 bool stateOpen = false;
 
-volatile byte interruptStateCounter = 1;
-
 void connectWiFi() {
   delay(10);
 
@@ -28,14 +26,13 @@ void connectWiFi() {
   WiFi.begin(wifi_ssid, wifi_password);
 
   Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     ESP.wdtFeed();
     Serial.print(".");
     interface.loop();
   }
-  
+
   Serial.println("");
   Serial.print("WiFi connected ");
   Serial.println(WiFi.localIP());
@@ -67,25 +64,27 @@ void connectMQTT() {
 // Interrupt handling
 // ##############################
 
-void interruptStateSR(){
-  interruptStateCounter++;
+uint64_t time_to_update = 0;
+
+void interruptStateSR() {
+  uint64_t time_to_update = millis() + 100;  //only after there have been no interrupts for 100ms will the state update
 }
 
-void interruptStateHandler(){
+void update_status() {
   static bool last = false;
 
   stateOpen = digitalRead(interruptStatePin) == LOW;
   state.setLocalSpaceState(stateOpen ? SpaceState::SOPEN : SpaceState::SCLOSED);
-  state.setRemoteSpaceState(stateOpen ? SpaceState::SOPEN : SpaceState::SCLOSED); //TODO query real remote state
+  state.setRemoteSpaceState(stateOpen ? SpaceState::SOPEN : SpaceState::SCLOSED);  //TODO query real remote state
 
-  if (last != stateOpen){
+  if (last != stateOpen) {
     StaticJsonBuffer<200> jsonBuffer;
 
     JsonObject& root = jsonBuffer.createObject();
     root["status"] = "ok";
 
     JsonObject& data = root.createNestedObject("data");
-    data["open"] = stateOpen?true:false;
+    data["open"] = stateOpen ? true : false;
 
     char message[200];
     root.printTo((char*)message, root.measureLength() + 1);
@@ -94,7 +93,7 @@ void interruptStateHandler(){
     last = stateOpen;
   }
 
-  interruptStateCounter = 0;
+  time_to_update = 0xFFFFFFFFFFFFFFFF;  //just set it to the max value of an uint64_t so it won't run again
 }
 
 // ##############################
@@ -103,12 +102,12 @@ void interruptStateHandler(){
 
 void setup() {
   // State switch
-  pinMode(interruptStatePin, INPUT_PULLUP); 
+  pinMode(interruptStatePin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptStatePin), interruptStateSR, CHANGE);
 
   state.setConnectionState(ConnectionState::PRE_SERIAL);
   Serial.begin(115200);
-  while (!Serial){
+  while (!Serial) {
     ESP.wdtFeed();
     interface.loop();
   };
@@ -116,7 +115,7 @@ void setup() {
   state.setConnectionState(ConnectionState::PRE_WIFI);
   connectWiFi();
 
-  mqttClient.setServer(mqtt_server, 1883);  
+  mqttClient.setServer(mqtt_server, 1883);
 }
 
 void loop() {
@@ -128,8 +127,8 @@ void loop() {
   }
   mqttClient.loop();
 
-  if (interruptStateCounter > 0){
-    interruptStateHandler();
+  if (millis() > time_to_update) {
+    update_status();
   }
 
   interface.loop();
